60b8d963b62adfd30127c85ac6111a711f6f1d35
TwoThousandDollars <bbrooks1@harding.edu>
2020-09-14 21:43:59 -0500

    completed findTheOldest exercise using the sort array method

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-let findTheOldest = function() {
-
+let findTheOldest = function(arr) {
+    return arr.sort((a, b) => {
+        if (!a.yearOfDeath || !b.yearOfDeath) {
+            a.yearOfDeath = new Date().getFullYear();
+            b.yearOfDeath = new Date().getFullYear();
+        }
+        const lastPerson = a.yearOfDeath - a.yearOfBirth;
+        const nextPerson = b.yearOfDeath - b.yearOfBirth;
+        return lastPerson > nextPerson ? -1 : 1;
+    })[0];
 }
 
 module.exports = findTheOldest
