c6831e64dc513932be35482402a9a68e4662ba46
TwoThousandDollars <bbrooks1@harding.edu>
2020-09-14 21:18:04 -0500

    finished fibonacci exercise

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(userNum) {
+    let num = parseInt(userNum);
+    if (num < 0) return "OOPS"
+    
+    let fibStart = [1, 1];
+    for (let i = fibStart.length; i < num; i++) {
+        fibStart.unshift(fibStart[0] + fibStart[1]);
+    }
+    return fibStart[0];
 }
 
 module.exports = fibonacci
